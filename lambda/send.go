ad4c61a611ea78cf2e9ce28df88b1386a5659af1
chenxxpro <chenxxpro@163.com>
2023-04-25 12:46:05 +0800

    init

---
+++ lambda/send.go
@@ -0,0 +1,28 @@
+package lambda
+
+import (
+    "encoding/base64"
+    "encoding/json"
+    "fmt"
+    "github.com/aws/aws-lambda-go/events"
+)
+
+func SendJSON(object any, statusCode int) (*events.APIGatewayV2HTTPResponse, error) {
+    bytes, err := json.Marshal(object)
+    if err != nil {
+        return nil, fmt.Errorf("response json, marshal error, %w", err)
+    }
+    return &events.APIGatewayV2HTTPResponse{
+        StatusCode:      statusCode,
+        Body:            base64.StdEncoding.EncodeToString(bytes),
+        IsBase64Encoded: true,
+    }, nil
+}
+
+func SendOK(data any) (*events.APIGatewayV2HTTPResponse, error) {
+    return SendJSON(data, 200)
+}
+
+func SendERR(error string, statusCode int) (*events.APIGatewayV2HTTPResponse, error) {
+    return SendJSON(map[string]string{"error": error}, statusCode)
+}
