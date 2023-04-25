1e502e68200970ef9fe3eab8a8be8fead572ebcf
chenyongjia <chenyj7@cmschina.com.cn>
2023-04-25 12:43:51 +0800

    init

---
+++ lambda/framework.go
@@ -0,0 +1,22 @@
+package lambda
+
+import (
+    "context"
+    "github.com/aws/aws-lambda-go/events"
+    "strings"
+)
+
+type HandleFunc func(ctx context.Context, request events.APIGatewayV2HTTPRequest) (*events.APIGatewayV2HTTPResponse, error)
+
+type MiddlewareFunc func(handler HandleFunc) HandleFunc
+
+func HeaderAuthorization(request *events.APIGatewayV2HTTPRequest) string {
+    bearer, ok := request.Headers["Authorization"]
+    if !ok {
+        return ""
+    }
+    if len(bearer) > 7 && strings.ToUpper(bearer[0:6]) == "BEARER" {
+        return bearer[7:]
+    }
+    return ""
+}
